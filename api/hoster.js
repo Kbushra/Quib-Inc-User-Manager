989f2bf3ba4a657b222f7ab5db779de33f8e2c86
kbushra <aaqibchoudhury3@gmail.com>
2026-07-05 21:12:35 +0100

    Fixed builds, moved SQL database to neon

--- api/hoster.js
+++ api/hoster.js
@@ -1,4 +1,4 @@
-import"express";import l from"express";import p from"argon2";import{Pool as w}from"pg";import h from"dotenv";h.config();var x=new w({connectionString:process.env.POSTGRES_URL,ssl:{rejectUnauthorized:!1}}),i=x;import d from"jsonwebtoken";import{rateLimit as R}from"express-rate-limit";function u(s){return`
+import x from"http";import l from"express";import p from"argon2";import{Pool as w}from"pg";import h from"dotenv";h.config();var R=new w({connectionString:process.env.POSTGRES_URL,ssl:{rejectUnauthorized:!1}}),i=R;import d from"jsonwebtoken";import{rateLimit as y}from"express-rate-limit";function u(s){return`
         <!doctype html>
         <html>
             <head>
@@ -29,7 +29,7 @@ import"express";import l from"express";import p from"argon2";import{Pool as w}fr
                 <script src=${s} type="module"></script>
             </body>
         </html>
-    `}function m(s){return d.sign(s,process.env.PRIVATE_KEY,{algorithm:"HS256"})}function y(s){return d.verify(s,process.env.PRIVATE_KEY,{algorithms:["HS256"]})}var o=l();o.use(l.static(process.cwd()));o.use(l.urlencoded({extended:!0}));o.use(l.json());o.get("/",(s,e)=>{e.status(200).send(u("/js-pages/main-page.js"))});o.get("/signup",(s,e)=>{e.status(200).send(u("/js-pages/signup-page.js"))});o.get("/login",(s,e)=>{e.status(200).send(u("/js-pages/login-page.js"))});o.get("/settings",(s,e)=>{e.status(200).send(u("/js-pages/settings-page.js"))});o.get("/*path",(s,e)=>{e.status(200).send(u("/js-pages/main-page.js"))});o.use((s,e,n)=>{let r=s.headers.authorization;if(!r||r.split(" ").length<=1){n();return}try{e.locals.token=r.split(" ")[1],e.locals.user=y(e.locals.token)}catch{e.sendStatus(401);return}n()});o.delete("/delete-account",(s,e)=>{console.log(`Deleting user ${e.locals.user.name}`),i.query(`
+    `}function m(s){return d.sign(s,process.env.PRIVATE_KEY,{algorithm:"HS256"})}function E(s){return d.verify(s,process.env.PRIVATE_KEY,{algorithms:["HS256"]})}var o=l();o.use(l.static(process.cwd()));o.use(l.urlencoded({extended:!0}));o.use(l.json());o.get("/",(s,e)=>{e.status(200).send(u("/js-pages/main-page.js"))});o.get("/signup",(s,e)=>{e.status(200).send(u("/js-pages/signup-page.js"))});o.get("/login",(s,e)=>{e.status(200).send(u("/js-pages/login-page.js"))});o.get("/settings",(s,e)=>{e.status(200).send(u("/js-pages/settings-page.js"))});o.get("/*path",(s,e)=>{e.status(200).send(u("/js-pages/main-page.js"))});o.use((s,e,n)=>{let r=s.headers.authorization;if(!r||r.split(" ").length<=1){n();return}try{e.locals.token=r.split(" ")[1],e.locals.user=E(e.locals.token)}catch{e.sendStatus(401);return}n()});o.delete("/delete-account",(s,e)=>{console.log(`Deleting user ${e.locals.user.name}`),i.query(`
         DELETE FROM users
         WHERE id = $1;
     `,[e.locals.user.id]),e.sendStatus(204)});o.post("/signup",async(s,e)=>{let{name:n,password:r}=s.body;if(console.log(`Signing up as ${n}`),!n){e.status(403).json({error:"Invalid username!"});return}if(!r){e.status(403).json({error:"Invalid password!"});return}if(n.length>255){e.status(403).json({error:"Username too long!"});return}if(r.length>255){e.status(403).json({error:"Password too long!"});return}if((await i.query(`
@@ -43,7 +43,7 @@ import"express";import l from"express";import p from"argon2";import{Pool as w}fr
         SELECT *
         FROM users
         WHERE name = $1;
-    `,[n]);if(t.rowCount===0){e.status(403).json({error:"Wrong username or password!",token:null,user:null});return}let a=t.rows[0];if(!await p.verify(a.hash_password,r)){e.status(403).json({error:"Wrong username or password!",token:null,user:null});return}let g={id:a.id,name:a.name,click_count:a.click_count};e.status(200).json({error:null,token:m(g),user:g})});o.put("/update-user",R({windowMs:1e3,limit:50}),async(s,e)=>{let r=e.locals.user,t=s.body;if(console.log(`Updating user ${t.name}`),Math.abs(t.click_count-r.click_count)>10||t.id!=r.id){e.status(403).json({error:"Illegal user modification!",token:null});return}let a=await i.query(`
+    `,[n]);if(t.rowCount===0){e.status(403).json({error:"Wrong username or password!",token:null,user:null});return}let a=t.rows[0];if(!await p.verify(a.hash_password,r)){e.status(403).json({error:"Wrong username or password!",token:null,user:null});return}let g={id:a.id,name:a.name,click_count:a.click_count};e.status(200).json({error:null,token:m(g),user:g})});o.put("/update-user",y({windowMs:1e3,limit:50}),async(s,e)=>{let r=e.locals.user,t=s.body;if(console.log(`Updating user ${t.name}`),Math.abs(t.click_count-r.click_count)>10||t.id!=r.id){e.status(403).json({error:"Illegal user modification!",token:null});return}let a=await i.query(`
         SELECT *
         FROM users
         WHERE name = $1;
@@ -55,5 +55,5 @@ import"express";import l from"express";import p from"argon2";import{Pool as w}fr
             UPDATE users
             SET hash_password = $1
             WHERE id = $2;
-        `,[c,t.id]),delete t.password}e.status(200).json({error:null,token:m(t)})});var f=o;var P=(s,e)=>f(s,e);export{P as default};
-//# sourceMappingURL=exporter.js.map
+        `,[c,t.id]),delete t.password}e.status(200).json({error:null,token:m(t)})});var f=o;var j=x.createServer(f);j.listen(3e3,()=>{console.log("Running app on localhost:3000")});
+//# sourceMappingURL=hoster.js.map
--- ts-scripts/database.ts
+++ ts-scripts/database.ts
@@ -2,5 +2,10 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const database: Pool = new Pool({ connectionString: process.env.POSTGRES_URL as string });
+const database: Pool = new Pool
+({
+    connectionString: process.env.POSTGRES_URL as string,
+    ssl: { rejectUnauthorized: false }
+});
+
 export default database;
